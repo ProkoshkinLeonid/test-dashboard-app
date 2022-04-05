3629cf947fada2375f6c313301409fdde316de77
Прокошкин Леонид <55481696+forjke777@users.noreply.github.com>
2022-04-05 23:41:18 +0300

    tiny fixes

--- front/src/Components/Table/Table.js
+++ front/src/Components/Table/Table.js
@@ -8,64 +8,7 @@ import {
     TableSearch, 
     TableTitleWrapper } from "./Table.styled"
 
-export const Table = () => {
-
-    const [data, setData] = useState([])
-    const [filteredData, setFilteredData] = useState([])
-    const [filter, setFilter] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-
-    const getData = (length) => {
-        function getRandomInt(max) {
-            return Math.floor(Math.random() * max);
-        }
-        let id = 1
-        setIsLoading(true)
-        setTimeout(() => setData(Array.from({ length }, () => {
-            setIsLoading(false)
-            return { 
-                id: id++, 
-                Summary1: getRandomInt(999), 
-                Summary2: getRandomInt(999), 
-                Summary3: getRandomInt(999), 
-                Summary4: getRandomInt(999), 
-                Summary5: getRandomInt(999)}
-        })), 3000)
-    }
-
-    const allFilterDataHandler = (e) => {
-       setTimeout(() => {
-        setFilter(e.target.value)
-        if(e.target.value) {
-                const newData = data.filter(num => (num.Summary1 || num.Summary2 || num.Summary3 || num.Summary4 || num.Summary5)  === +e.target.value)
-                setIsLoading(true)
-                setTimeout(() => {
-                    setFilteredData(newData)
-                    setIsLoading(false)
-                }, 1500)
-            }
-        }, 1300)
-    }
-
-    const filterByDataId = () => {
-        if (filteredData[0].id > filteredData[1].id) {
-            setFilteredData(prevState => [...prevState.sort((a,b) => a.id-b.id)])
-        } else {
-            setFilteredData(prevState => [...prevState.sort((a,b) => b.id-a.id)])
-        }
-    }
-
-    console.log(filteredData);
-
-    useEffect(() => {
-        getData(4)
-    }, [])
-
-    useEffect(() => {
-        if (!filter && !isLoading) {
-            setFilteredData(data)
-        }
-    }, [filter, data, isLoading])
+export const Table = ({filterByDataId, isLoading, filter, filteredData, allFilterDataHandler}) => {
 
     return <div>
         <TableTitleWrapper>Dashboard</TableTitleWrapper>
--- front/src/Pages/AnalyzePage/AnalyzePage.js
+++ front/src/Pages/AnalyzePage/AnalyzePage.js
@@ -1,10 +1,73 @@
+import { useEffect, useState } from "react"
 import { Rightbar } from "../../Components/Rightbar/Rightbar"
 import { Table } from "../../Components/Table/Table"
 import { AnalyzePageContainer } from "./AnalyzePage.styled"
 
 export const AnalyzePage = () => {
+
+    const [data, setData] = useState([])
+    const [filteredData, setFilteredData] = useState([])
+    const [filter, setFilter] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const getData = (length) => {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+        let id = 1
+        setIsLoading(true)
+        setTimeout(() => setData(Array.from({ length }, () => {
+            setIsLoading(false)
+            return { 
+                id: id++, 
+                Summary1: getRandomInt(999), 
+                Summary2: getRandomInt(999), 
+                Summary3: getRandomInt(999), 
+                Summary4: getRandomInt(999), 
+                Summary5: getRandomInt(999)}
+        })), 3000)
+    }
+
+    const allFilterDataHandler = (e) => {
+       setTimeout(() => {
+        setFilter(e.target.value)
+        if(e.target.value) {
+                const newData = data.filter(num => (num.Summary1 || num.Summary2 || num.Summary3 || num.Summary4 || num.Summary5)  === +e.target.value)
+                setIsLoading(true)
+                setTimeout(() => {
+                    setFilteredData(newData)
+                    setIsLoading(false)
+                }, 1500)
+            }
+        }, 1300)
+    }
+
+    const filterByDataId = () => {
+        if (filteredData[0].id > filteredData[1].id) {
+            setFilteredData(prevState => [...prevState.sort((a,b) => a.id-b.id)])
+        } else {
+            setFilteredData(prevState => [...prevState.sort((a,b) => b.id-a.id)])
+        }
+    }
+
+    useEffect(() => {
+        getData(4)
+    }, [])
+
+    useEffect(() => {
+        if (!filter && !isLoading) {
+            setFilteredData(data)
+        }
+    }, [filter, data, isLoading])
+
     return <AnalyzePageContainer>
      <Rightbar/>
-     <Table/>
+     <Table 
+        filterByDataId={filterByDataId} 
+        filter={filter}  
+        filteredData={filteredData} 
+        allFilterDataHandler={allFilterDataHandler} 
+        isLoading={isLoading}
+    />
     </AnalyzePageContainer>
 }
